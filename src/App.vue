5ced1f7fb65528f19f033bc3041288a8ed65d6dc
whoiscc <correctizer@gmail.com>
2020-09-04 01:00:11 +0800

    Init with wip Control component
    Signed-off-by: whoiscc <correctizer@gmail.com>

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="section">
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="column is-8">
+          <Control />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Control from "./components/Control.vue";
+
+export default {
+  name: "App",
+  components: {
+    Control,
+  },
+};
+</script>
---
+++ src/components/Control.vue
@@ -0,0 +1,50 @@
+<template>
+  <form>
+    <div class="field">
+      <label class="label">User name</label>
+      <div class="control">
+        <input type="text" class="input" />
+      </div>
+    </div>
+    <div class="field is-grouped" v-if="action != 'create'">
+      <div class="control">
+        <button class="button is-link is-light" @click.prevent="action = 'search'">Search</button>
+      </div>
+      <div class="control">
+        <button class="button is-link" @click.prevent="action = 'create'">Write</button>
+      </div>
+    </div>
+    <div class="field" v-if="action == 'create'">
+      <label class="label">Post</label>
+      <div class="control">
+        <textarea class="textarea" placeholder="Write your post" v-model="postContent"></textarea>
+      </div>
+    </div>
+    <div class="field is-grouped" v-if="action == 'create'">
+      <div class="control">
+        <button class="button is-link is-light" @click.prevent="discardPost">Discard</button>
+      </div>
+      <div class="control">
+        <button class="button is-link" @click.prevent="">Publish</button>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "Control",
+  data() {
+    return {
+      action: "undecided",
+      postContent: "",
+    };
+  },
+  methods: {
+    discardPost() {
+      this.action = "undecided";
+      this.postContent = "";
+    },
+  },
+};
+</script>
